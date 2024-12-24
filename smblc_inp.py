30eea10fd58476b9ff2b557ca2111b78b1d41b83
ceiba4687 <ceiba4687@gmail.com>
2024-12-24 16:42:21 +0800

    new main input

---
+++ smblc_inp.py
@@ -0,0 +1,6 @@
+# 读取输入数据
+
+
+def read_input(filename):
+    # 读取输入
+    print("Reading input data from", filename)
---
+++ smblc_main.py
@@ -0,0 +1,12 @@
+# 主函数
+import re
+from smblc_inp import read_input
+
+
+def main():
+    file_name = "data/smblc20230206_turkey_M77.inp"
+    read_input(file_name)
+
+
+if __name__ == "__main__":
+    main()
